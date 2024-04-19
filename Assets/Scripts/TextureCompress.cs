f381d52566993e9f6183ff86a41b7644eb2e8a4e
HeXueqiang <hexueqiang007@gmail.com>
2024-04-19 14:06:50 +0800

    update

--- Assets/Scripts/TextureCompress.cs
+++ Assets/Scripts/TextureCompress.cs
@@ -9,9 +9,8 @@ using Debug = UnityEngine.Debug;
 
 public class TextureCompress : MonoBehaviour 
 {
-    private string toolPath = "Path/To/astcenc-sse2.exe";
-    public Texture2D testTex2D;
-    private string pngPath = "Path/To/astcenc-sse2.exe";
+    private string toolPath;
+    private string pngPath;
 
     void Start()
     {
@@ -20,15 +19,13 @@ public class TextureCompress : MonoBehaviour
         //Debug.Log(Application.dataPath);
         //Debug.Log(Application.temporaryCachePath);
 
-        //pngPath = Path.Combine(Application.streamingAssetsPath, "RGBA32.png");
-        pngPath = "E:\\202404\\TextureCompressUnityProj\\Assets\\Textures\\ASTC\\98_OriRGBA32.png";
-
-
         string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
         toolPath = Path.Combine(appData, "MyTools/astcenc-sse2.exe");
-
         if (!File.Exists(toolPath))
             throw new FileNotFoundException($"Cannot find astc encoder at {Path.GetFullPath(toolPath)}.");
+
+        pngPath = Path.Combine(Application.streamingAssetsPath, "RGBA32.png");
+        //pngPath = "E:\\202404\\TextureCompressUnityProj\\Assets\\Textures\\ASTC\\98_OriRGBA32.png";
     }
 
     // Update is called once per frame
@@ -81,33 +78,4 @@ public class TextureCompress : MonoBehaviour
             }
         }
     }
-
-    //private void ASTC_Compress(string path)
-    //{
-    //    int extIndex = path.LastIndexOf('.');
-    //    string outputPath = path.Substring(0, extIndex) + "_astc" + path.Substring(extIndex);
-
-    //    ProcessStartInfo startInfo = new ProcessStartInfo
-    //    {
-    //        FileName = toolPath,
-    //        Arguments = $"-cl \"{path}\" \"{outputPath}\" 6x6 -medium",
-    //        UseShellExecute = false,
-    //        RedirectStandardOutput = true,
-    //        CreateNoWindow = true
-    //    };
-    //    Process process = new Process { StartInfo = startInfo };
-    //    process.Start();
-    //    process.WaitForExit();
-    //    if (process.ExitCode == 0)
-    //    {
-    //        Debug.Log("Texture compressed successfully.");
-            
-    //    }
-    //    else
-    //    {
-    //        Debug.LogError("Texture compression failed.");
-    //    }
-
-    //    process.Close();
-    //}
 }
